373ec2fee06aceb222209476e43018136b9db818
samhon <fishrain23@gmail.com>
2017-04-14 15:38:18 -0400

    finished yellow color filter. Next:canny edge

--- Virtual_Tumbler/Virtual_Tumbler/ViewController.mm
+++ Virtual_Tumbler/Virtual_Tumbler/ViewController.mm
@@ -29,8 +29,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    //Boolean VideoStream = true;
-    Boolean VideoStream = false;
+    Boolean VideoStream = true;
+    //Boolean VideoStream = false;
     [self VideoStillImage:VideoStream];
 }
 
@@ -58,12 +58,25 @@
         UIImage *image_test_3cards_dark = [UIImage imageNamed:@"image_test_3cards_dark.jpg"];
         if(image_test_many == nil) std::cout << "Cannot read in the file image_test_3cards_dark.jpg!!" << std::endl;
         
-        UIImage *inputImage = image_test_3cards;
+        //UIImage *inputImage = image_test_3cards;
+        //UIImage *inputImage = image_test_3cards_dark;
+        UIImage *inputImage = image_test_1card;
         
         // set the ImageView_ for still image
         [self showImage:inputImage];
         
-        imageView_.image = inputImage;
+        cv::Mat cvImage = [self cvMatFromUIImage:inputImage];
+        cv::Mat processImage;
+        cv::cvtColor(cvImage, processImage, CV_RGBA2BGR);
+        
+        // process image
+        
+        colorFilter(processImage);
+        
+        // show on the screen
+        imageView_.image = [self UIImageFromCVMat:processImage];
+        
+        //imageView_.image = inputImage;
     }
     
 }
@@ -160,5 +173,65 @@
     [self.view addSubview:imageView_];
 }
 
+// Member functions for converting from cvMat to UIImage
+- (cv::Mat)cvMatFromUIImage:(UIImage *)image
+{
+    CGColorSpaceRef colorSpace = CGImageGetColorSpace(image.CGImage);
+    CGFloat cols = image.size.width;
+    CGFloat rows = image.size.height;
+    
+    cv::Mat cvMat(rows, cols, CV_8UC4); // 8 bits per component, 4 channels (color channels + alpha)
+    
+    CGContextRef contextRef = CGBitmapContextCreate(cvMat.data,                 // Pointer to  data
+                                                    cols,                       // Width of bitmap
+                                                    rows,                       // Height of bitmap
+                                                    8,                          // Bits per component
+                                                    cvMat.step[0],              // Bytes per row
+                                                    colorSpace,                 // Colorspace
+                                                    kCGImageAlphaNoneSkipLast |
+                                                    kCGBitmapByteOrderDefault); // Bitmap info flags
+    
+    CGContextDrawImage(contextRef, CGRectMake(0, 0, cols, rows), image.CGImage);
+    CGContextRelease(contextRef);
+    
+    return cvMat;
+}
+// Member functions for converting from UIImage to cvMat
+-(UIImage *)UIImageFromCVMat:(cv::Mat)cvMat
+{
+    NSData *data = [NSData dataWithBytes:cvMat.data length:cvMat.elemSize()*cvMat.total()];
+    CGColorSpaceRef colorSpace;
+    
+    if (cvMat.elemSize() == 1) {
+        colorSpace = CGColorSpaceCreateDeviceGray();
+    } else {
+        colorSpace = CGColorSpaceCreateDeviceRGB();
+    }
+    
+    CGDataProviderRef provider = CGDataProviderCreateWithCFData((__bridge CFDataRef)data);
+    
+    // Creating CGImage from cv::Mat
+    CGImageRef imageRef = CGImageCreate(cvMat.cols,                                 //width
+                                        cvMat.rows,                                 //height
+                                        8,                                          //bits per component
+                                        8 * cvMat.elemSize(),                       //bits per pixel
+                                        cvMat.step[0],                            //bytesPerRow
+                                        colorSpace,                                 //colorspace
+                                        kCGImageAlphaNone|kCGBitmapByteOrderDefault,// bitmap info
+                                        provider,                                   //CGDataProviderRef
+                                        NULL,                                       //decode
+                                        false,                                      //should interpolate
+                                        kCGRenderingIntentDefault                   //intent
+                                        );
+    
+    
+    // Getting UIImage from CGImage
+    UIImage *finalImage = [UIImage imageWithCGImage:imageRef];
+    CGImageRelease(imageRef);
+    CGDataProviderRelease(provider);
+    CGColorSpaceRelease(colorSpace);
+    
+    return finalImage;
+}
 
 @end
--- Virtual_Tumbler/Virtual_Tumbler/cardRecognition.cpp
+++ Virtual_Tumbler/Virtual_Tumbler/cardRecognition.cpp
@@ -3,15 +3,30 @@
 void colorFilter(cv::Mat &image){
 
 
+    cv::Mat display_image;
+    cv::Mat gray_image;
     cv::Mat hsv_image;
+    cv::Mat canny_edge;
     cv::cvtColor(image, hsv_image, cv::COLOR_BGR2HSV);
+    cv::cvtColor(image, gray_image, cv::COLOR_BGR2GRAY);
     
     //keep yellow range color
-    //OpenCV HSV range is: H: 0 to 179 S: 0 to 255 V: 0 to 255
     
-    cv::Mat yellow_image;
-    cv::inRange(hsv_image, cv::Scalar(50, 125, 125), cv::Scalar(70, 125, 125), yellow_image);
-
-    cv::cvtColor(hsv_image, image, cv::COLOR_HSV2BGR);
+    cv::Mat yellow_mask;
+    cv::Mat mask;
+    
+    
+    // inRange will give 255 when inrange and 0 when not inrange
+    // H: yellow is around 35, S: larger means more saturated V: larger means darker
+    // OpenCV HSV range is: H: 0 to 179 S: 0 to 255 V: 0 to 255
+    cv::inRange(hsv_image, cv::Scalar(10, 150, 150), cv::Scalar(50, 200, 255), yellow_mask);
+    gray_image.copyTo(display_image, yellow_mask);
+    
+    // canny edge
+    //cv::blur(canny_edge, gray_image, cv::Size());
+    
+    
+    //cv::cvtColor(display_image, image, CV_BGR2RGBA);
+    cv::cvtColor(display_image, image, CV_GRAY2RGBA);
     
 }
