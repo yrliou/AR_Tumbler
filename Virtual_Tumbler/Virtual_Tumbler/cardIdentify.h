9139a1af20fb8f7d82bf6c11859bc90ffdb8da14
samhon <fishrain23@gmail.com>
2017-05-01 21:35:56 -0400

    finish card identify next: projection

---
+++ Virtual_Tumbler/Virtual_Tumbler/cardIdentify.h
@@ -0,0 +1,19 @@
+#ifndef __Virtual_Tumbler_cardIdentify__
+#define __Virtual_Tumbler_cardIdentify__
+
+#include <iostream>
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/nonfree/features2d.hpp"
+#include <opencv2/opencv.hpp>
+#include "armadillo"
+
+cv::Mat homographyinliner( std::vector<cv::KeyPoint> keypoints_database, cv::Mat descriptors_database, cv::Mat card_image , int &inlinernumber, cv::ORB *orb_detector_, cv::Mat &database_image);
+
+//cv::Mat homographyinliner( std::vector<cv::KeyPoint> keypoints_database, cv::Mat descriptors_database, cv::Mat card_image , int &inlinernumber, cv::BRISK *brisk_detector_, cv::Mat &database_image);
+
+cv::Mat DrawPts(cv::Mat &display_im, arma::fmat &pts, const cv::Scalar &pts_clr);
+cv::vector<cv::Point2f> Arma2Points2f(arma::fmat &pts);
+
+void projectImageTest(cv::Mat &firstframe, cv::Mat &secondframe, cv::ORB *orb_detector_, const char *model3dname);
+
+#endif
