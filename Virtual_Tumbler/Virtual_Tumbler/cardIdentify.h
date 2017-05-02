3627deacc0156ce030deee1ab6b11bdffec91614
samhon <fishrain23@gmail.com>
2017-05-02 19:37:25 -0400

    work on fixing bug in cardidentify

--- Virtual_Tumbler/Virtual_Tumbler/cardIdentify.h
+++ Virtual_Tumbler/Virtual_Tumbler/cardIdentify.h
@@ -16,6 +16,8 @@ cv::vector<cv::Point2f> Arma2Points2f(arma::fmat &pts);
 
 void projectImageTest(cv::Mat &firstframe, cv::Mat &secondframe, cv::ORB *orb_detector_, const char *model3dname);
 
-cv::vector<std::string>  findcardname(cv::vector<cv::vector<cv::KeyPoint>> keypoints_database, cv::vector<cv::Mat> descriptors_database, cv::Mat grayImage, float TRACK_RESCALE, cv::ORB *orb_detector_);
+cv::vector<int>  findcardname(cv::vector<cv::vector<cv::KeyPoint>> keypoints_database, cv::vector<cv::Mat> descriptors_database, cv::Mat grayImage, float TRACK_RESCALE, cv::ORB *orb_detector_, cv::vector<cv::vector<cv::Point>> card_corners, cv::vector<cv::Mat> &card_homography);
+
+cv::Mat findinlinerhomo(std::vector<cv::KeyPoint> keypoints_database, cv::Mat descriptors_database, std::vector<cv::KeyPoint> keypoints_inputimage, cv::Mat descriptors_inputimage, int &inlinernumber, cv::FlannBasedMatcher &matcher);
 
 #endif
