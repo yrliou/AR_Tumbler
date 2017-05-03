b78853646d540bf9f98975b905d8ec88c3d57119
Jocelyn Liu <yrliou@gmail.com>
2017-05-03 11:52:31 -0400

    matching multiple cards from database images.

--- Virtual_Tumbler/Virtual_Tumbler/cardIdentify.h
+++ Virtual_Tumbler/Virtual_Tumbler/cardIdentify.h
@@ -16,6 +16,8 @@ cv::vector<cv::Point2f> Arma2Points2f(arma::fmat &pts);
 
 void projectImageTest(cv::Mat &firstframe, cv::Mat &secondframe, cv::ORB *orb_detector_, const char *model3dname);
 
+void findKeypointsDescriptors(cv::Mat grayImage, cv::ORB *orb_detector_, cv::vector<cv::vector<cv::Point>> card_corners, cv::vector<cv::vector<cv::KeyPoint>>& keypoints, cv::vector<cv::Mat>& descriptors);
+
 cv::vector<int>  findcardname(cv::vector<cv::vector<cv::KeyPoint>> keypoints_database, cv::vector<cv::Mat> descriptors_database, cv::Mat grayImage, float TRACK_RESCALE, cv::ORB *orb_detector_, cv::vector<cv::vector<cv::Point>> card_corners, cv::vector<cv::Mat> &card_homography);
 
 cv::Mat findinlinerhomo(std::vector<cv::KeyPoint> keypoints_database, cv::Mat descriptors_database, std::vector<cv::KeyPoint> keypoints_inputimage, cv::Mat descriptors_inputimage, int &inlinernumber, cv::FlannBasedMatcher &matcher);
